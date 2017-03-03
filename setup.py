ea548933c740da738367bca689212d7d2ea724c5
Anton Ponomarenko <bear_ukraine@bigmir.net>
2017-03-03 21:52:57 +0200

    5th try: Fixed pip install problem with missing of 'static' folder

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-slideshows',
     version='0.1dev',
-    packages=['django-slideshows',],
+    packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.rst').read(),
 )
